793bfa228e0354c3c3f256d19d714f3c17f5c8af
Aadarsh <aadarsh-s@outlook.com>
2020-12-05 01:45:53 +0530

    Changing file system

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import "reflect-metadata";
 import { GraphQLServer } from 'graphql-yoga'
-import { resolvers } from "./resolver";
 import { createTypeormCon } from "./utils/createTypeORMCon";
+import { GraphQLSchema } from "graphql";
+import * as fs from 'fs'
+import * as path from 'path'
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { importSchema } from "graphql-import";
 
-
-const server = new GraphQLServer({ typeDefs: 'src/schema.graphql',  resolvers })
 
 export const startServer = async () => {
+    const schemas: GraphQLSchema[] = []
+    const folders = fs.readdirSync(path.join(__dirname,'./modules'))
+    folders.forEach(folder => {
+        const {resolvers} =  require(`./modules/${folder}/resolvers`)
+        const typeDefs = importSchema(path.join(__dirname, `./modules/${folder}/schema.graphql`));
+        schemas.push(makeExecutableSchema({resolvers,typeDefs}))  
+    })
+    const schema: any= mergeSchemas({schemas})
+    const server = new GraphQLServer({schema})
     await createTypeormCon();
     await server.start();
     console.log('Server is running on localhost:4000')
 }
+
 
 startServer()
---
+++ src/modules/register/resolvers.ts
@@ -0,0 +1,20 @@
+import { GQL } from "../../types/schema"
+import * as bcrypt from "bcrypt"
+import { User } from "../../entity/User";
+
+interface ResolverMap {
+  [key: string]: {
+    [key: string]: (parent:any,args: any, context: {}, info:any) => any
+  }
+}
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    register: async(_, {email,password}: GQL.IRegisterOnMutationArguments) => {
+      const hash = await bcrypt.hash(password, 10);
+      const user = await User.create({email, password: hash})
+      await user.save()
+      return true
+    }
+  }
+}
