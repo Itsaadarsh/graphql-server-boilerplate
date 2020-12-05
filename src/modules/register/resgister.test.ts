9e3deecc0e05149536cc52d29611455429673d42
Aadarsh <aadarsh-s@outlook.com>
2020-12-05 15:01:56 +0530

    Adding validation and new test cases

--- src/modules/register/resgister.test.ts
+++ src/modules/register/resgister.test.ts
@@ -11,23 +11,34 @@ afterAll(async() => {
   await getConnection().close()
 })
 
-const {email,password} = {email: "aadi@aadi.1com", password: "lololololololol"}
+const {email,password} = {email: "aadi@aadi.com", password: "q12312312w"}
+
 const host: string = 'http://localhost:4000/'
-const mutation = `
+
+const mutation = (testEmail: string,testPass: string) =>  `
     mutation {
-        register(email: "${email}", password: "${password}"){path}
+        register(email: "${testEmail}", password: "${testPass}"){path}
     }
 `
 
 test("Register test", async () => {
-    const req1 = await request(host, mutation)
+
+    // test to add user to db
+    const req1 = await request(host, mutation(email,password))
     expect(req1).toEqual({register: null})
     const users = await User.find({where: {email}})
     expect(users).toHaveLength(1)
     const user = users[0]
     expect(user.email).toEqual(email)
     expect(user.password).not.toEqual(password)
-    const req2 = await request(host, mutation)
+
+    // test to check for duplicate email's
+    const req2 = await request(host, mutation(email,password))
     expect(req2.register).toHaveLength(1);
     expect(req2.register[0].path).toEqual('email');
+    
+    // test the validatiom
+    const req3 = await request(host, mutation('1',password))
+    expect(req3.register).toHaveLength(2)
+
 })
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,6 +1,13 @@
+import * as yup from 'yup'
 import { GQL } from "../../types/schema"
 import * as bcrypt from "bcrypt"
 import { User } from "../../entity/User";
+import { yupErrorHandling } from '../../utils/yupErrorHanding';
+
+const schema = yup.object().shape({
+  email: yup.string().min(3).max(255).email(),
+  password: yup.string().min(6).max(255)
+})
 
 interface ResolverMap {
   [key: string]: {
@@ -10,7 +17,14 @@ interface ResolverMap {
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    register: async(_, {email,password}: GQL.IRegisterOnMutationArguments) => {
+    register: async(_, args: GQL.IRegisterOnMutationArguments) => {
+      try{
+        await schema.validate(args, {abortEarly: false})
+      }catch(err){
+        return yupErrorHandling(err)
+      }
+      
+      const {email,password} = args
       const findUser = await User.findOne({where: {email},select: ["id"]})
       if(findUser) {
         return [
