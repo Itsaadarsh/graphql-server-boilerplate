b38e95cf80c6a8a9fcc8eb9ff2f160b70c33c47f
Aadarsh <aadarsh-s@outlook.com>
2020-12-06 12:23:00 +0530

    Chaning index file

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,38 @@ import { importSchema } from "graphql-import";
 import { User } from "./entity/User";
 import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
 
-export const startServer = async () => {
-    const schemas: GraphQLSchema[] = []
-    const folders = fs.readdirSync(path.join(__dirname,'./modules'))
-    folders.forEach(folder => {
-        const {resolvers} =  require(`./modules/${folder}/resolvers`)
-        const typeDefs = importSchema(path.join(__dirname, `./modules/${folder}/schema.graphql`));
-        schemas.push(makeExecutableSchema({resolvers,typeDefs}))  
-    })
-    
-    const redis = new Redis()
-    const schema: any= mergeSchemas({schemas})
-    const server = new GraphQLServer({schema,context: ({request}) => ({
-        redis,
-        url: request.protocol + '://' + request.get("host")
-    } )})
+const schemas: GraphQLSchema[] = []
+const folders = fs.readdirSync(path.join(__dirname,'./modules'))
+folders.forEach(folder => {
+    const {resolvers} =  require(`./modules/${folder}/resolvers`)
+    const typeDefs = importSchema(path.join(__dirname, `./modules/${folder}/schema.graphql`));
+    schemas.push(makeExecutableSchema({resolvers,typeDefs}))  
+})
 
-    server.express.get('/confirm/:id',async (req,res) => {
-        const {id} = req.params
-        const userId = await redis.get(id)
-        if(userId){
-            await User.update({id:userId},{confirmed:true})
-            await redis.del(id)
-            res.send('Confirmed')
-        }
-        else{
-            res.send('Something went wrong!')
-        }
-    })
+const redis = new Redis()
+const schema: any= mergeSchemas({schemas})
+const server = new GraphQLServer({schema,context: ({request}) => ({
+    redis,
+    url: request.protocol + '://' + request.get("host")
+} )})
 
+server.express.get('/confirm/:id',async (req,res) => {
+    const {id} = req.params
+    const userId = await redis.get(id)
+    if(userId){
+        await User.update({id:userId},{confirmed:true})
+        await redis.del(id)
+        res.send('Confirmed')
+    }
+    else{
+        res.send('Something went wrong!')
+    }
+})
+
+const gettingStarted = async () => {
     await createTypeormCon();
     await server.start();
     console.log('Server is running on localhost:4000')
 }
 
-
-startServer()
+gettingStarted()
